022e76e46fba79a244b506ba184b8e4e34acb497
Ke Xu <kxu9@uci.edu>
2019-10-17 22:26:39 -0700

    test: inti

--- Inti.py
+++ Inti.py
@@ -1,3 +1,4 @@
+import fileinput
 def gcd(i, j):
     if i > j:
         return gcd(j, i)
@@ -13,5 +14,15 @@ def Inti(n, a, b):
             sum += i
     return sum
 
+stdin = []
+for i in fileinput.input():
+    stdin.append(i.rstrip())
 
-print(Inti(5, 1, 4))
+
+line = 1
+for testcase in range(int(stdin[0])):
+    n, a, b = stdin[line].split()
+    line += 1
+    print(Inti(n, a, b))
+
+# print(Inti(5, 1, 4))
