5ca61fbf0ea922a02f22970ffa0aaf66f5d6d847
Jack <jack@dhcp-10-8-001-222.mobile.reshsg.uci.edu>
2019-10-17 22:23:22 -0700

    input

--- dog.py
+++ dog.py
@@ -1,4 +1,7 @@
+import fileinput
 def compute(n, k, lst):
+    n = int(n)
+    k = int(k)
     if n <= k:
         return 0
     s = set(lst)
@@ -8,9 +11,21 @@ def compute(n, k, lst):
     i = 1
     diff = []
     while i < len(s):
-        diff.append(s[i] - s[i-1])
+        diff.append(int(s[i]) - int(s[i-1]))
         i += 1
     diff = sorted(diff)
-    return max(diff[:n-k])
+    return sum(diff[:len(s)-k])
 
-print(compute(5, 4, [30, 50, 41, 40, 20]))
+
+stdin = []
+for i in fileinput.input():
+    stdin.append(i.rstrip())
+
+
+line = 1
+for testcase in range(int(stdin[0])):
+    n, k = stdin[line].split()
+    line += 1
+    lst = stdin[line:line + int(n)]
+    line += int(n)
+    print(compute(n, k, lst))
