0320a6efbaf363403d689bf84a56e464dee99679
Jack <jack@dhcp-10-8-001-222.mobile.reshsg.uci.edu>
2019-10-17 22:39:27 -0700

    dog final

---
+++ dog2.py
@@ -0,0 +1,17 @@
+tC = int(input())
+for t in range(tC):
+    n,k = [int(i) for i in input().split()]
+    dogs = set()
+    diffs = []
+    for j in range(n):
+        dogs.add(int(input()))
+
+    dogs = sorted(list(dogs))
+    for j in range(len(dogs)-1):
+        diffs.append(dogs[j+1] - dogs[j])
+
+    diffs = sorted(diffs)
+    if k == 1:
+        print(dogs[-1] - dogs[0])
+    else:
+        print(sum(diffs[:len(dogs)-k]))
